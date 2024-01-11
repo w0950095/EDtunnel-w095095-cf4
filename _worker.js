e9ea0eb2608c26d472c81642e25761521708ccf8
w0950095 <143480597+w0950095@users.noreply.github.com>
2024-01-12 01:41:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '84c983bc-36c7-4a5f-9fd7-2beec4cade9a';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ['work1.liumingan.cloudns.biz','work2.liumingan.cloudns.biz', 'work3.liumingan.cloudns.biz', 'work4.liumingan.cloudns.biz', 'work5.liumingan.cloudns.biz', 'work6.liumingan.cloudns.biz'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
